859b424b9394d534b7ff2acb5dd34ff96fba3887
Eli Bishop <eli@launchdarkly.com>
2018-12-08 11:20:33 -0800

    indents

--- src/main/java/com/launchdarkly/client/dynamodb/DynamoDBFeatureStoreCore.java
+++ src/main/java/com/launchdarkly/client/dynamodb/DynamoDBFeatureStoreCore.java
@@ -82,224 +82,224 @@ class DynamoDBFeatureStoreCore implements FeatureStoreCore {
     this.prefix = "".equals(prefix) ? null : prefix;
   }
   
-    @Override
-    public void close() throws IOException {
-      client.shutdown();
-    }
+  @Override
+  public void close() throws IOException {
+    client.shutdown();
+  }
 
-    @Override
-    public <T extends VersionedData> T getInternal(VersionedDataKind<T> kind, String key) {
-        GetItemResult result = getItemByKeys(namespaceForKind(kind), key);
-        return unmarshalItem(kind, result.getItem());
-    }
+  @Override
+  public <T extends VersionedData> T getInternal(VersionedDataKind<T> kind, String key) {
+      GetItemResult result = getItemByKeys(namespaceForKind(kind), key);
+      return unmarshalItem(kind, result.getItem());
+  }
 
-    @Override
-    public <T extends VersionedData> Map<String, T> getAllInternal(VersionedDataKind<T> kind) {
-      Map<String, T> itemsOut = new HashMap<>();
-    for (QueryResult result: paginateQuery(makeQueryForKind(kind))) {
-      for (Map<String, AttributeValue> item: result.getItems()) {
-        T itemOut = unmarshalItem(kind, item);
-        if (itemOut != null) {
-          itemsOut.put(itemOut.getKey(), itemOut);
-        }
+  @Override
+  public <T extends VersionedData> Map<String, T> getAllInternal(VersionedDataKind<T> kind) {
+    Map<String, T> itemsOut = new HashMap<>();
+  for (QueryResult result: paginateQuery(makeQueryForKind(kind))) {
+    for (Map<String, AttributeValue> item: result.getItems()) {
+      T itemOut = unmarshalItem(kind, item);
+      if (itemOut != null) {
+        itemsOut.put(itemOut.getKey(), itemOut);
       }
     }
-    return itemsOut;
-    }
+  }
+  return itemsOut;
+  }
 
-    @Override
-    public void initInternal(Map<VersionedDataKind<?>, Map<String, ? extends VersionedData>> allData) {
-      // Start by reading the existing keys; we will later delete any of these that weren't in allData.
-      Set<Map.Entry<String, String>> unusedOldKeys = readExistingKeys(allData.keySet());
-        
-      List<WriteRequest> requests = new ArrayList<>();
-      int numItems = 0;
+  @Override
+  public void initInternal(Map<VersionedDataKind<?>, Map<String, ? extends VersionedData>> allData) {
+    // Start by reading the existing keys; we will later delete any of these that weren't in allData.
+    Set<Map.Entry<String, String>> unusedOldKeys = readExistingKeys(allData.keySet());
       
-      // Insert or update every provided item
-      for (Map.Entry<VersionedDataKind<?>, Map<String, ? extends VersionedData>> entry: allData.entrySet()) {
-        VersionedDataKind<?> kind = entry.getKey();
-        for (VersionedData item: entry.getValue().values()) {         
-          Map<String, AttributeValue> encodedItem = marshalItem(kind, item);
-          requests.add(new WriteRequest(new PutRequest(encodedItem)));
-          
-          Map.Entry<String, String> combinedKey = new AbstractMap.SimpleEntry<>(
-              namespaceForKind(kind), item.getKey());
-          unusedOldKeys.remove(combinedKey);
-          
-          numItems++;
-        }
-      }
-      
-      // Now delete any previously existing items whose keys were not in the current data
-      for (Map.Entry<String, String> combinedKey: unusedOldKeys) {
-        if (!combinedKey.getKey().equals(initedKey())) {
-          Map<String, AttributeValue> keys = ImmutableMap.of(
-              partitionKey, new AttributeValue(combinedKey.getKey()),
-              sortKey, new AttributeValue(combinedKey.getValue()));
-          requests.add(new WriteRequest(new DeleteRequest(keys)));
-        }
-      }
-      
-      // Now set the special key that we check in initializedInternal()
-      Map<String, AttributeValue> initedItem = ImmutableMap.of(
-        partitionKey, new AttributeValue(initedKey()),
-        sortKey, new AttributeValue(initedKey()));
-      requests.add(new WriteRequest(new PutRequest(initedItem)));
-      
-      batchWriteRequests(client, tableName, requests);
-      
-      logger.info("Initialized table {} with {} items", tableName, numItems);
-    }
-
-    @Override
-    public <T extends VersionedData> T upsertInternal(VersionedDataKind<T> kind, T item) {
+    List<WriteRequest> requests = new ArrayList<>();
+    int numItems = 0;
+    
+    // Insert or update every provided item
+    for (Map.Entry<VersionedDataKind<?>, Map<String, ? extends VersionedData>> entry: allData.entrySet()) {
+      VersionedDataKind<?> kind = entry.getKey();
+      for (VersionedData item: entry.getValue().values()) {         
         Map<String, AttributeValue> encodedItem = marshalItem(kind, item);
+        requests.add(new WriteRequest(new PutRequest(encodedItem)));
         
-        if (updateHook != null) { // instrumentation for tests
-          updateHook.run();
-        }
+        Map.Entry<String, String> combinedKey = new AbstractMap.SimpleEntry<>(
+            namespaceForKind(kind), item.getKey());
+        unusedOldKeys.remove(combinedKey);
         
-        try {
-          PutItemRequest put = new PutItemRequest(tableName, encodedItem);
-          put.setConditionExpression("attribute_not_exists(#namespace) or attribute_not_exists(#key) or :version > #version");
-          put.addExpressionAttributeNamesEntry("#namespace", partitionKey);
-          put.addExpressionAttributeNamesEntry("#key", sortKey);
-          put.addExpressionAttributeNamesEntry("#version", versionAttribute);
-          put.addExpressionAttributeValuesEntry(":version", new AttributeValue().withN(String.valueOf(item.getVersion())));
-          client.putItem(put);
-        } catch (ConditionalCheckFailedException e) {
-          // The item was not updated because there's a newer item in the database.
-          // We must now read the item that's in the database and return it, so CachingStoreWrapper can cache it.
-          return getInternal(kind, item.getKey());
-        }
-        
-        return item;
+        numItems++;
+      }
     }
+    
+    // Now delete any previously existing items whose keys were not in the current data
+    for (Map.Entry<String, String> combinedKey: unusedOldKeys) {
+      if (!combinedKey.getKey().equals(initedKey())) {
+        Map<String, AttributeValue> keys = ImmutableMap.of(
+            partitionKey, new AttributeValue(combinedKey.getKey()),
+            sortKey, new AttributeValue(combinedKey.getValue()));
+        requests.add(new WriteRequest(new DeleteRequest(keys)));
+      }
+    }
+    
+    // Now set the special key that we check in initializedInternal()
+    Map<String, AttributeValue> initedItem = ImmutableMap.of(
+      partitionKey, new AttributeValue(initedKey()),
+      sortKey, new AttributeValue(initedKey()));
+    requests.add(new WriteRequest(new PutRequest(initedItem)));
+    
+    batchWriteRequests(client, tableName, requests);
+    
+    logger.info("Initialized table {} with {} items", tableName, numItems);
+  }
 
-    @Override
-    public boolean initializedInternal() {
-    GetItemResult result = getItemByKeys(initedKey(), initedKey());
-    return result.getItem() != null && result.getItem().size() > 0;
-    }
-    
-    public void setUpdateHook(Runnable updateHook) {
-      this.updateHook = updateHook;
-    }
-    
-    private String prefixedNamespace(String base) {
-      return prefix == null ? base : (prefix + ":" + base);
-    }
-    
-    private String namespaceForKind(VersionedDataKind<?> kind) {
-      return prefixedNamespace(kind.getNamespace());
-    }
-    
-    private String initedKey() {
-      return prefixedNamespace("$inited");
-    }
-    
-    private QueryRequest makeQueryForKind(VersionedDataKind<?> kind) {
-    Condition cond = new Condition();
-      cond.setComparisonOperator(ComparisonOperator.EQ);
-      cond.setAttributeValueList(ImmutableList.of(new AttributeValue(namespaceForKind(kind))));
+  @Override
+  public <T extends VersionedData> T upsertInternal(VersionedDataKind<T> kind, T item) {
+      Map<String, AttributeValue> encodedItem = marshalItem(kind, item);
+      
+      if (updateHook != null) { // instrumentation for tests
+        updateHook.run();
+      }
+      
+      try {
+        PutItemRequest put = new PutItemRequest(tableName, encodedItem);
+        put.setConditionExpression("attribute_not_exists(#namespace) or attribute_not_exists(#key) or :version > #version");
+        put.addExpressionAttributeNamesEntry("#namespace", partitionKey);
+        put.addExpressionAttributeNamesEntry("#key", sortKey);
+        put.addExpressionAttributeNamesEntry("#version", versionAttribute);
+        put.addExpressionAttributeValuesEntry(":version", new AttributeValue().withN(String.valueOf(item.getVersion())));
+        client.putItem(put);
+      } catch (ConditionalCheckFailedException e) {
+        // The item was not updated because there's a newer item in the database.
+        // We must now read the item that's in the database and return it, so CachingStoreWrapper can cache it.
+        return getInternal(kind, item.getKey());
+      }
+      
+      return item;
+  }
 
-      QueryRequest req = new QueryRequest(tableName);
-      req.setConsistentRead(true);
-      req.addKeyConditionsEntry(partitionKey, cond);
-      return req;
+  @Override
+  public boolean initializedInternal() {
+  GetItemResult result = getItemByKeys(initedKey(), initedKey());
+  return result.getItem() != null && result.getItem().size() > 0;
+  }
+  
+  public void setUpdateHook(Runnable updateHook) {
+    this.updateHook = updateHook;
+  }
+  
+  private String prefixedNamespace(String base) {
+    return prefix == null ? base : (prefix + ":" + base);
+  }
+  
+  private String namespaceForKind(VersionedDataKind<?> kind) {
+    return prefixedNamespace(kind.getNamespace());
+  }
+  
+  private String initedKey() {
+    return prefixedNamespace("$inited");
+  }
+  
+  private QueryRequest makeQueryForKind(VersionedDataKind<?> kind) {
+  Condition cond = new Condition();
+    cond.setComparisonOperator(ComparisonOperator.EQ);
+    cond.setAttributeValueList(ImmutableList.of(new AttributeValue(namespaceForKind(kind))));
+
+    QueryRequest req = new QueryRequest(tableName);
+    req.setConsistentRead(true);
+    req.addKeyConditionsEntry(partitionKey, cond);
+    return req;
+  }
+  
+  private GetItemResult getItemByKeys(String namespace, String key) {
+    Map<String, AttributeValue> keyMap = ImmutableMap.of(
+      partitionKey, new AttributeValue(namespace),
+      sortKey, new AttributeValue(key)
+  );
+    GetItemRequest req = new GetItemRequest(tableName, keyMap, true);
+    return client.getItem(req);
+  }
+  
+  private Set<Map.Entry<String, String>> readExistingKeys(Iterable<VersionedDataKind<?>> kinds) {
+    Set<Map.Entry<String, String>> keys = new HashSet<>();
+    for (VersionedDataKind<?> kind: kinds) {
+      QueryRequest req = makeQueryForKind(kind);
+      req.setProjectionExpression("#namespace, #key");
+      req.addExpressionAttributeNamesEntry("#namespace", partitionKey);
+      req.addExpressionAttributeNamesEntry("#key", sortKey);
+      for (QueryResult result: paginateQuery(req)) {
+        for (Map<String, AttributeValue> item: result.getItems()) {
+          String namespace = item.get(partitionKey).getS();
+          String key = item.get(sortKey).getS();
+          keys.add(new AbstractMap.SimpleEntry<>(namespace, key));
+        }
+      }
     }
-    
-    private GetItemResult getItemByKeys(String namespace, String key) {
-      Map<String, AttributeValue> keyMap = ImmutableMap.of(
-        partitionKey, new AttributeValue(namespace),
-        sortKey, new AttributeValue(key)
+    return keys;
+  }
+  
+  private Map<String, AttributeValue> marshalItem(VersionedDataKind<?> kind, VersionedData item) {
+    String json = FeatureStoreHelpers.marshalJson(item);
+    return ImmutableMap.of(
+      partitionKey, new AttributeValue(namespaceForKind(kind)),
+      sortKey, new AttributeValue(item.getKey()),
+      versionAttribute, new AttributeValue().withN(String.valueOf(item.getVersion())),
+      itemJsonAttribute, new AttributeValue(json)
     );
-      GetItemRequest req = new GetItemRequest(tableName, keyMap, true);
-      return client.getItem(req);
+  }
+  
+  private <T extends VersionedData> T unmarshalItem(VersionedDataKind<T> kind, Map<String, AttributeValue> item) {
+    if (item == null || item.size() == 0) {
+      return null;
     }
-    
-    private Set<Map.Entry<String, String>> readExistingKeys(Iterable<VersionedDataKind<?>> kinds) {
-      Set<Map.Entry<String, String>> keys = new HashSet<>();
-      for (VersionedDataKind<?> kind: kinds) {
-        QueryRequest req = makeQueryForKind(kind);
-        req.setProjectionExpression("#namespace, #key");
-        req.addExpressionAttributeNamesEntry("#namespace", partitionKey);
-        req.addExpressionAttributeNamesEntry("#key", sortKey);
-        for (QueryResult result: paginateQuery(req)) {
-          for (Map<String, AttributeValue> item: result.getItems()) {
-            String namespace = item.get(partitionKey).getS();
-            String key = item.get(sortKey).getS();
-            keys.add(new AbstractMap.SimpleEntry<>(namespace, key));
-          }
-        }
+    AttributeValue jsonAttr = item.get(itemJsonAttribute);
+    if (jsonAttr == null || jsonAttr.getS() == null) {
+      throw new IllegalStateException("DynamoDB map did not contain expected item string");
+    }
+    return FeatureStoreHelpers.unmarshalJson(kind, jsonAttr.getS());
+  }
+  
+  Iterable<QueryResult> paginateQuery(final QueryRequest request) {
+    return new Iterable<QueryResult>() {
+      public Iterator<QueryResult> iterator() {
+        return new QueryIterator(request);
       }
-      return keys;
+    };
+  }
+  
+  private class QueryIterator implements Iterator<QueryResult> {
+    private boolean eof;
+    private QueryRequest request;
+    
+    QueryIterator(QueryRequest request) {
+      this.request = request;
     }
     
-    private Map<String, AttributeValue> marshalItem(VersionedDataKind<?> kind, VersionedData item) {
-      String json = FeatureStoreHelpers.marshalJson(item);
-      return ImmutableMap.of(
-        partitionKey, new AttributeValue(namespaceForKind(kind)),
-        sortKey, new AttributeValue(item.getKey()),
-        versionAttribute, new AttributeValue().withN(String.valueOf(item.getVersion())),
-        itemJsonAttribute, new AttributeValue(json)
-      );
-    }
-    
-    private <T extends VersionedData> T unmarshalItem(VersionedDataKind<T> kind, Map<String, AttributeValue> item) {
-      if (item == null || item.size() == 0) {
-        return null;
-      }
-      AttributeValue jsonAttr = item.get(itemJsonAttribute);
-      if (jsonAttr == null || jsonAttr.getS() == null) {
-        throw new IllegalStateException("DynamoDB map did not contain expected item string");
-      }
-      return FeatureStoreHelpers.unmarshalJson(kind, jsonAttr.getS());
-    }
-    
-    Iterable<QueryResult> paginateQuery(final QueryRequest request) {
-      return new Iterable<QueryResult>() {
-        public Iterator<QueryResult> iterator() {
-          return new QueryIterator(request);
-        }
-      };
-    }
-    
-    private class QueryIterator implements Iterator<QueryResult> {
-      private boolean eof;
-      private QueryRequest request;
-      
-      QueryIterator(QueryRequest request) {
-        this.request = request;
-      }
-      
-    @Override
-    public boolean hasNext() {
-      return !eof;
-    }
+  @Override
+  public boolean hasNext() {
+    return !eof;
+  }
 
-    @Override
-    public QueryResult next() {
-      if (eof) {
-        return null;
-      }
-      QueryResult result = client.query(request);
-      if (result.getLastEvaluatedKey() != null) {
-        request.setExclusiveStartKey(result.getLastEvaluatedKey());
-      } else {
-        eof = true;
-      }
-      return result;
+  @Override
+  public QueryResult next() {
+    if (eof) {
+      return null;
     }
+    QueryResult result = client.query(request);
+    if (result.getLastEvaluatedKey() != null) {
+      request.setExclusiveStartKey(result.getLastEvaluatedKey());
+    } else {
+      eof = true;
     }
-    
-    static void batchWriteRequests(AmazonDynamoDB client, String tableName, List<WriteRequest> requests) {
-      int batchSize = 25;
-      for (int i = 0; i < requests.size(); i += batchSize) {
-        int limit = (i + batchSize < requests.size()) ? (i + batchSize) : requests.size();
-        List<WriteRequest> batch = requests.subList(i, limit); 
-        Map<String, List<WriteRequest>> batchMap = ImmutableMap.of(tableName, batch);
-        client.batchWriteItem(batchMap);
-      }
+    return result;
+  }
+  }
+  
+  static void batchWriteRequests(AmazonDynamoDB client, String tableName, List<WriteRequest> requests) {
+    int batchSize = 25;
+    for (int i = 0; i < requests.size(); i += batchSize) {
+      int limit = (i + batchSize < requests.size()) ? (i + batchSize) : requests.size();
+      List<WriteRequest> batch = requests.subList(i, limit); 
+      Map<String, List<WriteRequest>> batchMap = ImmutableMap.of(tableName, batch);
+      client.batchWriteItem(batchMap);
     }
+  }
 }
