dde011f7b997911754346eebed99bc7533baf580
Eli Bishop <eli@launchdarkly.com>
2021-09-13 19:10:55 -0700

    use Releaser v2 config + newer CI images

--- .ldrelease/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ue
-
-# Publish to Sonatype
-echo "Publishing to Sonatype"
-./gradlew publishToSonatype closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
